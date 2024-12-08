6a37ba2a8722b83b8df5330c68e87eb26fe3141b
Paweł Królicki <zeusjoan@nosgov.home>
2024-12-08 07:29:34 +0100

    Added Vercel configuration files and updated requirements

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
+# main/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/home.html', {
+        'title': 'Home'
+    })
